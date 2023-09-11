68e52f1454ef8f8ea9da31af717e13d51cb71fc0
Дмитрий <dfeskov1998@mail.ru>
2023-09-11 22:32:49 +0300

    add new func and docstring

--- str_func.py
+++ str_func.py
@@ -1,4 +1,12 @@
 def get_str():
+    """Возвращает строку с заглавными буквами"""
 
     data = input().upper()
+    return data
+
+
+def receive_new_func():
+    """Принимает строку с первой заглавной буквой"""
+
+    data = input().title()
     return data
